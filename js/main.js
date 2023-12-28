81fb15e619d45690385bd467c896a04cd919e4f5
silviosnjr <silviosnjr@gmail.com>
2023-12-28 09:13:44 -0300

    funções

---
+++ js/main.js
@@ -0,0 +1,11 @@
+    const roboYellow = document.querySelector("#roboYellow");
+    roboYellow.addEventListener("click", function(){
+        console.log("cliquei no robô");
+    });
+
+    function digaOi(){
+        console.log("oi");
+        console.log("Bem vindo a oficina de RoboTron");
+    }
+
+    digaOi();
